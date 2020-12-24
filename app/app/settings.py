204a91e4af12e8a1d7309edf491eb760ee18f402
Rijin NP <rijinswaminathan@gmail.com>
2020-12-24 15:24:14 +0530

    model and its test cases

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'app.core',
-]
+    'core',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
